acdb9887e7175cf1a21bbf43417f924337026da7
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-06-07 07:43:41 +0000

    Update Bullhead fingerprint to OPM6.171019.030.B1/4768815
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- invictrix.mk
+++ invictrix.mk
@@ -33,5 +33,5 @@ INVICTRIX_MAINTAINER = anirudhgupta109
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.B1 4768815 release-keys"
